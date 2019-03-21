a572ef0026f280ece31fa303b1203c047a00e593
Chukwuemeke Obuseh <emeke.obuseh@gmail.com>
2019-03-21 15:40:07 +0100

    loginAdmin api link update

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -72,7 +72,7 @@ return this.http.get<Staff[]>(this.baseUrl + "staffs");
 
 
 loginAdmin(username: string, password: string): Observable<Login> {
-    return this.http.post<Login>('http://192.168.1.29/biometric%20system/admin/admin_login.php',
+    return this.http.post<Login>('http://localhost/biometric_system/admin/admin_login.php',
                                   {username, password},
                                   httpOptions)
           .pipe(
